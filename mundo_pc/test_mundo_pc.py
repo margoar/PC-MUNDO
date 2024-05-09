6e2767d00a8bbcf3f94fb52640d4bfb825da7b4d
Marcela <mgonzalez@puntoticket.cl>
2024-05-09 11:56:25 -0400

    nuevos objetos

--- mundo_pc/test_mundo_pc.py
+++ mundo_pc/test_mundo_pc.py
@@ -12,20 +12,20 @@ teclado2 = Teclado("HP","USB")
 raton2 = Raton("HP","USB")
 monitor2 = Monitor("HP",15)
 
+teclado3= Teclado("Gamer", 'Bluetotth')
+raton3 = Raton('gamer' ,'Bluetoht')
+monitor3 = Monitor ("Gamer",34)
+
 cp1 = Computadora('HP-Monster', monitor1,teclado1,raton1)
 #print(cp1)
 cp2 = Computadora('HP-Junior', monitor2,teclado2,raton2)
-
-
-
-
-
-teclado2 = Teclado("ACER","Bl")
-raton2 = Raton("HP","USB")
-monitor2 = Monitor("HP",15)
-
+cp3 = Computadora("Gammer",monitor3,teclado3,raton3)
 
 computadoras1 = [cp1, cp2]
-
 orden = Orden(computadoras1)
+orden.agregar_computadora(cp3)
 print(orden)
+
+computadoras2 = [cp2,cp3]
+orden2 = Orden(computadoras2)
+print(orden2)
